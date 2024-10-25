286645f2a909ff18e7c3ccdcd3bbd4f3dc4c918d
Lewis Price <1995ldp@gmail.com>
2024-10-25 08:55:43 +0100

    feat: adds test data

---
+++ data/data.py
@@ -0,0 +1,18 @@
+quotes = [
+    {
+        "content":"The secret of success is constancy to purpose.",
+        "author":"Benjamin Disraeli"
+    },
+    {
+        "content":"Trouble is only opportunity in work clothes.",
+        "author":"Henry J. Kaiser"
+    },
+    {
+        "content":"All our talents increase in the using, and every faculty, both good and bad, strengthen by exercise.",
+        "author":"Anne Brontë"
+    },
+    {
+        "content":"Don't leave a stone unturned. It's always something, to know you have done the most you could.",
+        "author":"Charles Dickens"
+    }
+]
